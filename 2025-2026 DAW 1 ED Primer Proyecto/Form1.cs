ebbe73b76ce501545c5031b7b7c5229614b4b3eb
RicardoSornosa <RicardoSornosa@DESKTOP-9GRD8KR>
2025-10-01 12:20:58 +0200

    Agregar archivos de proyecto.

---
+++ 2025-2026 DAW 1 ED Primer Proyecto/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2025_2026_DAW_1_ED_Primer_Proyecto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
